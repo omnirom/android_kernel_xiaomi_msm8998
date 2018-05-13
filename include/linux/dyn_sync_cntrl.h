d9a2afd3113b3b61b182d7ac9a272063ed67811b
Wight554 <wight554@gmail.com>
2018-05-13 19:50:00 +0300

    fs/dyn_sync_cntrl: Enable by default

--- include/linux/dyn_sync_cntrl.h
+++ include/linux/dyn_sync_cntrl.h
@@ -1,11 +1,11 @@
-/* 
+/*
  * Dynamic sync control driver definitions (V2)
- * 
+ *
  * by andip71 (alias Lord Boeffla)
- * 
+ *
  */
 
-#define DYN_FSYNC_ACTIVE_DEFAULT false
+#define DYN_FSYNC_ACTIVE_DEFAULT true
 #define DYN_FSYNC_VERSION_MAJOR 2
 #define DYN_FSYNC_VERSION_MINOR 1
 
